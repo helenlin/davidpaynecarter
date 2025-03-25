6fb038e5ba836df5bda034bf56b31c445f3072b0
Helen <lin.helen517@gmail.com>
2025-03-25 15:02:02 -0400

    update with assets

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-document.getElementById("riosVideo").addEventListener("click", function () {
+const video = document.getElementById("riosVideo");
+video.volume = 0.09; // Sets the volume to 20%
+
+video.addEventListener("click", function () {
   if (this.paused) {
     this.play();
   } else {
